0984e7e7e6f9f5ef042c0fffe9b99f5353689917
Phase 3 <Phase 3@Y7000-B>
2019-04-13 09:26:27 -0500

    Commit 6

---
+++ AP/src/johnShop/OrderTest.java
@@ -0,0 +1,27 @@
+package johnShop;
+
+public class OrderTest {
+	private  int window;
+	private float closewin;
+	private String Test;
+	public int getWindow() {
+		return window;
+	}
+	public void setWindow(int window) {
+		this.window = window;
+	}
+	public float getClosewin() {
+		return closewin;
+	}
+	public void setClosewin(float closewin) {
+		this.closewin = closewin;
+	}
+	public String getTest() {
+		return Test;
+	}
+	public void setTest(String test) {
+		Test = test;
+	}
+	
+
+}
